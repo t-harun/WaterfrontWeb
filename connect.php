77340a41961434c1d6a3403a894314ff184aa4f1
t-harun <77989825+t-harun@users.noreply.github.com>
2021-10-20 12:08:32 -0400

    Split Code and Save cart using Session

--- connect.php
+++ connect.php
@@ -1,59 +1,75 @@
 <?php
 //start session
 session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "waterfrontdb";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
-    echo 'Hello, world fail.';
-    die("Connection failed: " . mysqli_connect_error());
+require_once ('CreateDb.php');
+require_once ('component.php');
+// create instance of Createdb class
+$database = new CreateDb("waterfrontdb", "products");
+
+
+
+if (isset($_POST['add']) || isset($_POST['submit'])){
+    if(isset($_SESSION['cart'])){
+
+        $item_array_id = array_column($_SESSION['cart'], "product_id");
+
+        if(in_array($_POST['product_id'], $item_array_id)){
+            echo "<script>alert('Product is already added in the cart..!')</script>";
+            echo "<script>window.location = 'connect.php'</script>";
+        }else{
+
+            $count = count($_SESSION['cart']);
+            $item_array = array(
+                'product_id' => $_POST['product_id']
+            );
+
+            $_SESSION['cart'][$count] = $item_array;
+        }
+
+    }else{
+        $item_array = array(
+            'product_id' => $_POST['product_id']
+        );
+
+        // Create new session variable
+        $_SESSION['cart'][0] = $item_array;
+        print_r($_SESSION['cart']);
+    }
 }
 
-$sql = "SELECT * FROM products";
-$result = mysqli_query($conn, $sql);
 
-if(isset($_POST['submit']) || !empty($_POST['submit'])){
-    $product_id = $_POST['product_id'];
-    $sql = "INSERT INTO shopping_cart SET
-        product_id = '$product_id'
-    ";
-    $conn = mysqli_connect($servername, $username, $password, $dbname) or die(mysqli_error());
-    $res = mysqli_query($conn,$sql)or die(mysqli_error());
-}
 ?>
+
 <!doctype html>
 <html>
 
 <head>
-    <title> Test Page</title>
+    <title>Shopping Cart</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 </head>
 
 <body>
-<div class = "col-md">
-    <div class="row">
-        <h2 class = "text-center">Top Products</h2><br><br>
-        <a href="cart.php" class="btn btn-white">Buy</a>
+
+<?php
+require_once ('header.php');
+?>
+<div class="container">
+    <div class="row text-center py-5">
         <?php
-            while($row = mysqli_fetch_array($result)):
+        $result = $database->getData();
+        while ($row = mysqli_fetch_assoc($result)){
+            component($row['name'], $row['price'], $row['image'], $row['id']);
+        }
         ?>
-        <form action="connect.php" method="POST">
-        <div class="col-md">
-                <h4><?= $row['name'];?></h4>
-                <img class="img-fluid img-thumbnail" src="<?= $row['image'];?>" alt="<?= $row['image'];?>">
-                <p>$<?= $row['price'];?></p>
-                <p><?= $row['description'];?></p>
-                <input type="submit" name="submit" class="btn btn-success" value="Add to Cart" />
-                <input type="hidden" name="product_id" value="<?= $row['id'];?>">
-        </div>
-        </form>
-        <?php endwhile;?>
     </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
